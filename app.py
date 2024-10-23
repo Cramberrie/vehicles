499c2a20da0c676cc6be2bef32661f183781a2f4
Cramberrie <aldogon145@gmail.com>
2024-10-22 20:28:00 -0600

    app

--- app.py
+++ app.py
@@ -3,13 +3,25 @@ import pandas as pd
 import plotly.express as px
 
 
-st. header("Grafico de burbujas para venta de coches")
+st. header("Grafico de conjunto de datos para venta de coches")
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir grafico de burbujas')  # crear un botón
+hist_button = st.checkbox('Construir histograma')  # crear un botón
+scatter_button = st.checkbox('Construir grafico de burbujas')
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un grafico de burbujas para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para coches con transmision automatica')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+if scatter_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de burbujas para coches con transmision manual')
 
     # crear un grafico de burbujas
     fig = px.scatter(car_data.query("transmission=='manual'"),
